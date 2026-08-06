10a5902c9e19b48efbc771c1d6af3b7fab9c56b5
darshan <darshanzala369@gmail.com>
2026-08-06 18:44:04 +0530

    Add audible urgent notification alerts

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,10 @@
 self.addEventListener("push", (event) => {
   let data = { title: "PanditConnect", body: "You have a new update.", url: "/" };
   try { data = { ...data, ...event.data.json() }; } catch {}
-  event.waitUntil(self.registration.showNotification(data.title, { body: data.body, icon: "/favicon.svg", badge: "/favicon.svg", data: { url: data.url }, tag: data.eventType || "panditconnect-update", renotify: true }));
+  event.waitUntil(Promise.all([
+    self.registration.showNotification(data.title, { body: data.body, icon: "/favicon.svg", badge: "/favicon.svg", data: { url: data.url }, tag: data.eventType || "panditconnect-update", renotify: true, silent: false, requireInteraction: data.eventType === "BOOKING_REQUESTED", vibrate: [180, 80, 180] }),
+    clients.matchAll({ type: "window", includeUncontrolled: true }).then((windows) => windows.forEach((client) => client.postMessage({ type: "PANDITCONNECT_PUSH", eventType: data.eventType }))),
+  ]));
 });
 self.addEventListener("notificationclick", (event) => {
   event.notification.close();
